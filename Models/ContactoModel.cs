7b996eaf7a1c8e3f99ce79bb44b9478862dd09fd
AlanSiquina <44984135+AlanSiquina@users.noreply.github.com>
2024-07-04 16:12:22 -0600

    Add files via upload

---
+++ Models/ContactoModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CRUDCORE.Models
+{
+    public class ContactoModel
+    {
+        public int IdContacto { get; set; }
+        [Required (ErrorMessage ="El campo nombre es obligatorio")]
+        public string? Nombre { get; set; }
+        [Required (ErrorMessage ="El campo Teléfono es obligatorio")]
+        public string? Telefono { get; set; }
+        [Required (ErrorMessage ="El campo correo es obligatorio")]  
+        public string? Correo { get; set; }
+    }
+}
